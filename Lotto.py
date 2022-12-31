cb258d6335c9cb3a5c2feb1866b5c57b188c5d85
Kuba <kubaszczyk.kuba@gmail.com>
2022-12-31 13:03:46 +0100

    petla counter

--- Lotto.py
+++ Lotto.py
@@ -6,12 +6,14 @@ Totek_liczby = range(1, 50)
 
 wylosowane_liczby = []
 counter = 0
+print(counter)
 
 while wylosowane_liczby != moje_liczby:
     wylosowane_liczby = sample(Totek_liczby, 6)
-    counter =+ 1
+    counter += 1
 
 print(counter)    
+print('dupa')
  #   if moje_liczby == wylosowane_liczby:
   #      print('wygrałeś')
    # else:

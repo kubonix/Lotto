f8d8053ed4eace859b4a89c3382c62cd7667719e
Kuba <kubaszczyk.kuba@gmail.com>
2022-12-31 12:51:45 +0100

    petla z printem

--- Lotto.py
+++ Lotto.py
@@ -1,3 +1,22 @@
 from random import sample
 
-moje 
+moje_liczby = [7, 11, 13, 23, 33, 45]
+
+Totek_liczby = range(1, 50)
+
+wylosowane_liczby = []
+counter = 0
+
+while wylosowane_liczby != moje_liczby:
+    wylosowane_liczby = sample(Totek_liczby, 6)
+    counter =+ 1
+
+print(counter)    
+ #   if moje_liczby == wylosowane_liczby:
+  #      print('wygrałeś')
+   # else:
+    #    print('graj dalej')
+
+print(wylosowane_liczby)
+
+

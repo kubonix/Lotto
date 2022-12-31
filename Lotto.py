f67f142f4959b568752e492ffc2c7c7bca83a25a
Kuba <kubaszczyk.kuba@gmail.com>
2022-12-31 13:20:14 +0100

    koniec

--- Lotto.py
+++ Lotto.py
@@ -1,24 +1,20 @@
 from random import sample
 
-moje_liczby = [7, 11, 13, 23, 33, 45]
+moje_liczby = [7, 11, 23, 33, 39,45]
 
 Totek_liczby = range(1, 50)
 
 wylosowane_liczby = []
 counter = 0
-print(counter)
+#print(counter)
 
 while wylosowane_liczby != moje_liczby:
-    wylosowane_liczby = sample(Totek_liczby, 6)
+    wylosowane_liczby = sample(Totek_liczby, 3)
     counter += 1
 
-print(counter)    
-print('dupa')
- #   if moje_liczby == wylosowane_liczby:
-  #      print('wygrałeś')
-   # else:
-    #    print('graj dalej')
-
+#print(counter)    
 print(wylosowane_liczby)
+
+print('Wygrałbyś szóstkę za ' + str(counter) + ' razem')
 
 
